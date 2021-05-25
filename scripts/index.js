6c4e92183de54f51066ad6d2e338eefad79c6470
Patrik Mrnka <u19.416@pslib.cloud>
2021-05-25 11:29:33 +0200

    :(

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+var queryParams = new URLSearchParams(window.location.search);
+var a = queryParams.get("a");
+var b = queryParams.get("b");
+var c = queryParams.get("c");
 
 // Vymaže obsah elementu
 const clearElement = (element) => {
@@ -25,10 +29,21 @@ const drawLines = (lAB, lBC, lCA, coordA, coordB, coordC) => {
 
 // vypočítá a doplní zbývající hodnoty v reakci na stisk tlačítka a vyplnění hodnot
 const calculate = (e) => {
-    // ---
+    document.getElementById("sideA").value = a;
+    document.getElementById("sideB").value = b;
+    document.getElementById("sideC").value = c;
+    var Perimeter = a + b + c;
+    var halfPerimeter = (Perimeter / 2);
+    var alpha = 2 * Math.asin(Math.sqrt(((halfPerimeter - b)*(halfPerimeter - c)) / (b * c)));
+    var beta = 2 * Math.asin(Math.sqrt(((halfPerimeter - c)*(halfPerimeter - a)) / (a * c)));
+    var gamma = 2 * Math.asin(Math.sqrt(((halfPerimeter - a)*(halfPerimeter - b)) / (a * b)));
+    var halfPerimeter
+    document.getElementById("alpha").value = alpha;
+    document.getElementById("beta").value = beta;
+    document.getElementById("gamma").value = gamma;
 }
 
 // inicializační kód
 const init = () => {
-    // ---
+    calculate();
 }
